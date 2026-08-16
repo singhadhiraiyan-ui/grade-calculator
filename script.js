66176dc6e0787ac8a720aa5180b9e9adad93330e
singhadhiraiyan-ui <singhadhiraiyan@gmail.com>
2026-08-16 14:52:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const calculateButton = document.getElementById("calculateButton");
 const result = document.getElementById("result");
 
 calculateButton.addEventListener("click", function() {
-    const grade = gradeInput.value;
-    result.textContent = "Deine Note ist: " + grade;
+    const grade = Number(gradeInput.value);
+
+    if (grade >= 1 && grade <= 6) {
+        result.textContent = "Deine Note ist: " + grade;
+    } else {
+        result.textContent = "Bitte gib eine Note zwischen 1 und 6 ein.";
+    }
 });
