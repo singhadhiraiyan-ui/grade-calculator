6efdaf38aae75a6168d0b1a9ed7a8e5b54f1672b
singhadhiraiyan-ui <singhadhiraiyan@gmail.com>
2026-08-16 14:47:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gradeInput = document.getElementById("grade");
 const calculateButton = document.getElementById("calculateButton");
 const result = document.getElementById("result");
-calculateButton.addEventListener("click", function()
+
+calculateButton.addEventListener("click", function() {
+    const grade = gradeInput.value;
+    result.textContent = "Deine Note ist: " + grade;
+});
